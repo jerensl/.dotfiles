f9a798a3a224da015168d26f1623b687a1f8f66a
jerensl <54782057+jerensl@users.noreply.github.com>
2022-08-09 08:55:59 +0800

    update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,44 @@ starship init fish | source
 
 # set the workspace path
 set -x GOPATH $HOME/go
+set -x GO111MODULE on
 
 # add the go bin path to be able to execute our programs
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 
 # direnv config
 direnv hook fish | source
+direnv export fish | source
+
+# exac for list directory
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
+
+set -U fish_color_autosuggestion      brblack
+set -U fish_color_cancel              -r
+set -U fish_color_command             brgreen
+set -U fish_color_comment             brmagenta
+set -U fish_color_cwd                 green
+set -U fish_color_cwd_root            red
+set -U fish_color_end                 brmagenta
+set -U fish_color_error               brred
+set -U fish_color_escape              brcyan
+set -U fish_color_history_current     --bold
+set -U fish_color_host                normal
+set -U fish_color_match               --background=brblue
+set -U fish_color_normal              normal
+set -U fish_color_operator            cyan
+set -U fish_color_param               brblue
+set -U fish_color_quote               yellow
+set -U fish_color_redirection         bryellow
+set -U fish_color_search_match        'bryellow' '--background=brblack'
+set -U fish_color_selection           'white' '--bold' '--background=brblack'
+set -U fish_color_status              red
+set -U fish_color_user                brgreen
+set -U fish_color_valid_path          --underline
+set -U fish_pager_color_completion    normal
+set -U fish_pager_color_description   yellow
+set -U fish_pager_color_prefix        'white' '--bold' '--underline'
+set -U fish_pager_color_progress      'brwhite' '--background=cyan'
